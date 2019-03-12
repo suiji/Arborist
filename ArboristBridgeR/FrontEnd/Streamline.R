b1de1a4213ce450fc178ec406a35fb58813eec8c
Seligman <seligman@localhost.localdomain>
2019-03-12 14:33:27 -0700

    Revises documentation according to check_win() advisory.

--- ArboristBridgeR/FrontEnd/Streamline.R
+++ ArboristBridgeR/FrontEnd/Streamline.R
@@ -15,5 +15,5 @@
 ## You should have received a copy of the GNU General Public License
 ## along with ArboristBridgeR.  If not, see <http://www.gnu.org/licenses/>.
 "Streamline" <-
-"Streamline" <- function(rb)
+"Streamline" <- function(rs)
     UseMethod("Streamline")
